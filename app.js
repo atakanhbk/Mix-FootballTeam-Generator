dec545b30d16c9a29fdae89c10c58dd80c6198fb
atakanhbk <atakanhbk@gmail.com>
2024-02-13 23:29:38 +0300

    Change algo

--- app.js
+++ app.js
@@ -8,29 +8,45 @@ const secondTeamMembers = document.getElementsByClassName(
 
 const seperateNamesToList = () => {
   const namesList = nameTextArea.value.split(",");
+
   createTeams(namesList);
 };
 
 const createTeams = (namesList) => {
+  firstTeamMembers.innerHTML = "";
+  secondTeamMembers.innerHTML = "";
+  const firstTeam = [];
+  const secondTeam = [];
   const namesListLength = namesList.length;
 
   for (let i = 0; i < namesListLength; i++) {
     const randomNumber = Math.floor(Math.random() * namesList.length);
     const selectedName = namesList.splice(randomNumber, 1)[0]; // Rastgele seçilen ismi listeden kaldır
-    if (i % 2 === 0) {
-      createTeamMemberNames(1, selectedName);
-    } else {
-      createTeamMemberNames(2, selectedName);
+    if (selectedName != "") {
+      if (i % 2 === 0) {
+        firstTeam.push(selectedName);
+      } else {
+        secondTeam.push(selectedName);
+      }
     }
   }
+
+  console.log("First Team:", firstTeam);
+  console.log("Second Team:", secondTeam);
+
+  createTeamMemberNames(firstTeam, secondTeam);
 };
 
-const createTeamMemberNames = (whichTeam, selectedName) => {
-  const teamMemberName = document.createElement("h3");
-  teamMemberName.textContent = `${selectedName}`;
-  if (whichTeam === 1) {
+const createTeamMemberNames = (firstTeam, secondTeam) => {
+  for (let i = 0; i < firstTeam.length; i++) {
+    const teamMemberName = document.createElement("h3");
+    teamMemberName.textContent = `${firstTeam[i]}`;
     firstTeamMembers.appendChild(teamMemberName);
-  } else {
+  }
+
+  for (let i = 0; i < secondTeam.length; i++) {
+    const teamMemberName = document.createElement("h3");
+    teamMemberName.textContent = `${secondTeam[i]}`;
     secondTeamMembers.appendChild(teamMemberName);
   }
 };
