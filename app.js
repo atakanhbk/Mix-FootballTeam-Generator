6a077abafed95d2dc0970d9ace6628fc027f916b
atakanhbk <atakanhbk@gmail.com>
2024-02-13 23:02:02 +0300

    Created Generate half of part

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-const generateBtn = document.getElementsByClassName("generateBtn");
+const generateBtn = document.getElementsByClassName("generateBtn")[0];
+const nameTextArea = document.getElementsByClassName("names")[0];
 
-console.log(generateBtn);
+const clickedGenerateBtn = () => {
+  const namesList = nameTextArea.value.split(",");
+  createTeams(namesList);
+};
+
+const createTeams = (namesList) => {
+  const firstTeam = [];
+  const secondTeam = [];
+  const namesListLength = namesList.length;
+
+  for (let i = 0; i < namesListLength; i++) {
+    const randomNumber = Math.floor(Math.random() * namesList.length);
+    const selectedName = namesList.splice(randomNumber, 1)[0]; // Rastgele seçilen ismi listeden kaldır
+    if (i % 2 === 0) {
+      firstTeam.push(selectedName);
+    } else {
+      secondTeam.push(selectedName);
+    }
+  }
+
+  console.log("First Team:", firstTeam);
+  console.log("Second Team:", secondTeam);
+};
+generateBtn.addEventListener("click", clickedGenerateBtn);
