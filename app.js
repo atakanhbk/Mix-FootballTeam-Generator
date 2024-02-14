0c32bdeb2febd6e8d8ef97def3367bb2ecdf749c
atakanhbk <atakanhbk@gmail.com>
2024-02-14 17:56:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const playerNumber = document.getElementsByClassName("playerNumber")[0];
 const playerNames = document.getElementsByClassName("player-names")[0];
 const generateBtn = document.getElementsByClassName("generateBtn")[0];
 
-
 const selectOptionFunction = () => {
   playerNames.innerHTML = "";
   const playerNumberInt = parseInt(playerNumber.value);
@@ -13,7 +12,7 @@ const selectOptionFunction = () => {
     <input class="player-name">
     <br>
     <span>Player Value = </span>
-    <select class="playerValue" name="playerValue">
+    <select class="player-value" name="playerValue">
       <option value="5">5</option>
       <option value="6">6</option>
       <option value="7">7</option>
@@ -28,8 +27,20 @@ const selectOptionFunction = () => {
   }
 };
 
-
-
+const generateTeam = () => {
+  const playerNames = document.querySelectorAll(".player-name");
+  const playerValues = document.querySelectorAll(".player-value");
+  let playersObject = [];
+  const randomTeamSelector = Math.floor(Math.random()*2);
+  console.log(randomTeamSelector);
+  for (let i = 0; i < playerNames.length; i++) {
+    const player = {
+      name: playerNames[i].value,
+      value: playerValues[i].value,
+    };
+    playersObject.push(player);
+  }
+};
 
 playerNumber.addEventListener("change", selectOptionFunction);
 generateBtn.addEventListener("click", generateTeam);
